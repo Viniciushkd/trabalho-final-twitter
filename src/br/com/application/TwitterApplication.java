139d24e1c8ec1b3cc3039b69f68afb6e08da0bcb
Marcos Vinicius <viniciushkd@gmail.com>
2018-08-19 19:16:54 -0300

    Trabalho Final Fundamento Plataforma Java e UML

--- src/br/com/application/TwitterApplication.java
+++ src/br/com/application/TwitterApplication.java
@@ -2,38 +2,72 @@ package br.com.application;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import br.com.bo.TwitterController;
 import br.com.tweet.ControleTweet;
 import br.com.tweet.Tweet;
-import twitter4j.Query;
-import twitter4j.QueryResult;
-import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 
-public class App {
+public class TwitterApplication {
 
+	private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+	private static final LocalDateTime dateNow = LocalDateTime.now();
+	private static final Twitter twitter = TwitterFactory.getSingleton();
+	
 	public static void main(String[] args) {
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-		LocalDateTime dateNow = LocalDateTime.now();
-		Twitter twitter = TwitterFactory.getSingleton();
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e1) {
+			e1.printStackTrace();
+		}
 		
+		while (true) {
+			HashMap<String, Integer> hm = new HashMap<>();
+			hm.put("Buscar pela hashtag...", 1);
+			hm.put("Post Tweet", 2);
+			Object[] twitter = hm.keySet().toArray();
+			
+			String opcao = (String) JOptionPane.showInputDialog(null, "Opções", "Twitter", JOptionPane.DEFAULT_OPTION, null,
+					twitter, null);
+			
+			if (opcao == null)
+				break;
+			
+			switch (hm.get(opcao)) {
+			case 1:
+				String texto = JOptionPane.showInputDialog("Buscar pela hashtag:");
+				buscaTweet(texto.replace("#", ""));
+				break;
+
+			case 2:
+				postTweet();
+				break;
+			}
+		}
+	}
+	
+//  BUSCA TWEET
+	private static void buscaTweet(String busca) {
+		TwitterController twitterController = new TwitterController();
 		HashMap<String , ControleTweet> hm = new HashMap<>();
 		
-		
-//      BUSCA TWEET
 		for(int i=7;i>0;i--) {
 			LocalDateTime dateLast = LocalDateTime.of(dateNow.getYear(), dateNow.getMonth(), dateNow.getDayOfMonth() - i-1, dateNow.getHour(), dateNow.getMinute());
 			LocalDateTime dateAfter = LocalDateTime.of(dateNow.getYear(), dateNow.getMonth(), dateNow.getDayOfMonth() - i, dateNow.getHour(), dateNow.getMinute());
 			String dateLastFormatt = dateLast.format(dtf);
 			String dateAfterFormatt = dateAfter.format(dtf);
 			
-			hm.put(dateAfterFormatt, buscaTwitter("#java9 since:" + dateLastFormatt + " until:" + dateAfterFormatt, twitter));
+			hm.put(dateAfterFormatt, twitterController.buscaTwitter("#" + busca + " since:" + dateLastFormatt + " until:" + dateAfterFormatt, twitter));
 		}
 		
 		for(String i : hm.keySet()) {
@@ -63,49 +97,17 @@ public class App {
 			System.out.println("Primeiro da lista: " + listaTweets.stream().findFirst().get().getNome());
 			System.out.println("Ultimo da lista: " + listaTweets.get(listaTweets.size()-1)+"\n");
 		}
-		
-		
-//		POST TWEET
-//		try {
-//			postTweet("Trabalho Final Fundamento Plataforma Java e UML - @michelpf", twitter);
-//		} catch (TwitterException e) {
-//			e.printStackTrace();
-//		}
+
 	}
 
-	private static ControleTweet buscaTwitter(String busca, Twitter twitter) {
+//	POST TWEET
+	private static void postTweet() {
+		TwitterController twitterController = new TwitterController();
 		try {
-			ControleTweet controle = new ControleTweet();
-			Query query = new Query(busca);
-			query.setCount(150);
-			QueryResult result = twitter.search(query);
-			List<Tweet> tweets  = new ArrayList<>();
-
-			int countReTweet = 0, countFavorited = 0;
-			for (Status status : result.getTweets()) {
-				Tweet tweet = new Tweet();
-				tweet.setNome(status.getUser().getName());
-				tweet.setData(status.getCreatedAt());
-				tweet.setTweet(status.getText());
-				countReTweet = status.isRetweet() ? countReTweet + 1 : countReTweet + 0;
-				countFavorited = status.isFavorited() ? countFavorited + 1 : countFavorited + 0; // ? Retorna tudo falso
-				tweets.add(tweet);
-			}
-			
-			controle.setListTweet(tweets);
-			controle.setQuantidade(result.getTweets().size());
-			controle.setQuantidadeReTweet(countReTweet);
-			controle.setQuantidadeFavorited(countFavorited);
-			
-			return controle;
-		} catch (Exception e) {
+			twitterController.postTweet("Trabalho Final Fundamento Plataforma Java e UML - @michelpf", twitter);
+		} catch (TwitterException e) {
 			e.printStackTrace();
 		}
-		return null;
 	}
-	
-	private static void postTweet(String texto, Twitter twitter) throws TwitterException {
-	    Status status = twitter.updateStatus(texto);
-	    System.out.println("Successfully updated the status to [" + status.getText() + "].");
-	}
+
 }
